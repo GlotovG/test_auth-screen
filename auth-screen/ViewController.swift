8bb850bff389dee1dbe8ecf87207f807e8c17499
Glotov Gennady <glotovg@gmail.com>
2018-09-02 23:54:28 +0300

    Экран авторизации + переход на экран для восстановления логина/пароля или регистрации нового пользователя

--- auth-screen/ViewController.swift
+++ auth-screen/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var userLogin: UITextField!
+    @IBOutlet weak var userPassword: UITextField!
+    @IBOutlet weak var registrationBtn: UIButton!
+    @IBOutlet weak var loginBtn: UIButton!
+    @IBOutlet weak var forgotLoginBtn: UIButton!
+    @IBOutlet weak var forgotPasswordBtn: UIButton!
+    
+    
+    var myInformation = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        registrationBtn.layer.cornerRadius = 5
+        loginBtn.layer.cornerRadius = 5
+        loginBtn.isEnabled = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = myInformation
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func loginPressed(_ sender: Any) {
+        if (userLogin.text?.isEmpty)! || (userPassword.text?.isEmpty)!{
+            //ничего не делать, но можно вывести сообщение о необходимости заполнить логин и пароль
+        } else {
+            if let userName = userLogin.text {
+                myInformation = "Привет, " + userName
+                performSegue(withIdentifier: "login", sender: nil)
+            }
+        }
     }
-
-
+    
+    @IBAction func enrollPressed(_ sender: Any) {
+        myInformation = "Регистрация"
+        performSegue(withIdentifier: "login", sender: nil)
+    }
+    
+    @IBAction func forgotLoginPressed(_ sender: Any) {
+        myInformation = "Забыли логин?"
+        performSegue(withIdentifier: "login", sender: nil)
+    }
+    
+    @IBAction func forgotPassPressed(_ sender: Any) {
+        myInformation = "Забыли пароль?"
+        performSegue(withIdentifier: "login", sender: nil)
+    }
+    
 }
-
